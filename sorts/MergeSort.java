e34e24129c5163bc84c59f1ba595aec732c618df
tcc6g7 <tcc6g7@mst.edu>
2013-11-30 21:49:00 -0600

    added reset/abort code to merge sort

--- sorts/MergeSort.java
+++ sorts/MergeSort.java
@@ -1,7 +1,6 @@
 package code.sorts;
 
 import java.awt.*;
-import java.util.Arrays;
 
 /**
  * @author Thomas Clay
@@ -13,29 +12,25 @@ public class MergeSort extends Sort {
     super(values, delay, "Merge Sort");
   }
 
-  private void sort2(){
+  private void sort(){
     for(int width = 1; width < values.length; width *= 2){
-//      System.out.println("width = " + width);
       for(int i = 0; i < values.length; i = i + 2 * width){
         if(!running) return;
         int leftStart = i;
         int rightStart =  Math.min(i+width, values.length);
         int end = Math.min(width*2 + i, values.length);
-//        System.out.println("leftStart = " + leftStart);
-//        System.out.println("rightStart = " + rightStart);
-//        System.out.println("end = " + end);
-        Rectangle[] sorted = merge2(leftStart, rightStart, end);
+        Rectangle[] sorted = merge(leftStart, rightStart, end);
         assertSorted(sorted);
-        move2(leftStart, end, sorted);
+        move(leftStart, end, sorted);
       }
     }
   }
 
 
 
-  private void move2(int start, int end, Rectangle[] sorted){
+  private void move(int start, int end, Rectangle[] sorted){
     for(int m = start; m < end; m++){
-//      System.out.println("bar[" + m + "] = sorted[" + (m - start) + "]");
+      if(!running) return;
       colorBar(m, Color.GREEN);
       swap(bars[m], sorted[m - start]);
     }
@@ -43,25 +38,23 @@ public class MergeSort extends Sort {
   }
 
 
-  private Rectangle[] merge2(int leftStart, int rightStart, int end){
+  private Rectangle[] merge(int leftStart, int rightStart, int end){
     int left = leftStart;
     int right = rightStart;
     Rectangle[] array = new Rectangle[end - leftStart];
     int j = 0;
     while(j < array.length){
-//      System.out.println("j = " + j);
+      if(!running) return null;
       //if right is out OR left has some and left is less than right
       colorBar(left, Color.RED);
       colorBar(right,Color.RED);
       repaint();
       if(!(right < end) || (left < rightStart && compare(left, right) < 0)){
         //move left indexValue into next array position
-//        System.out.println("moving left");
         array[j] = new Rectangle(bars[left]);
         left++;
       }
       else{
-//        System.out.println("moving right");
         array[j] = new Rectangle(bars[right]);
         right++;
       }
@@ -72,18 +65,9 @@ public class MergeSort extends Sort {
     return array;
   }
 
-  protected int colorCompare(int barIndex1, int barIndex2){
-    colorBar(barIndex1, Color.RED);
-    colorBar(barIndex2, Color.RED);
-    repaint();
-    uncolorBar(barIndex1);
-    uncolorBar(barIndex2);
-    return compare(barIndex1, barIndex2);
-  }
-
 
   protected void runSort() {
-    sort2();
+    sort();
   }
 
 
