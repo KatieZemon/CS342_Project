52e927a8525b0846d88138effab55e2d5c35310e
Thomas C <tcc6g7@mst.edu>
2013-12-02 22:58:09 -0600

    added documentation

--- MessageDisplayHelper.java
+++ MessageDisplayHelper.java
@@ -4,11 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
+ * Assists with displaying messages to the user
  * @author Thomas
- *         Date and Time: 12/2/13 10:39 PM
  */
 public class MessageDisplayHelper {
 
+  /**
+   * Shows the user a pop-up error message
+   * @param parent component who produced the error message
+   * @param message the message to display
+   */
   public static void showErrorMessage(Component parent, String message){
     JOptionPane.showMessageDialog(parent, message, "Error", JOptionPane.ERROR_MESSAGE);
   }
