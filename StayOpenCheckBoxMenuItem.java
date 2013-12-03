e7955ec1d44589b20b100b66ed690dd122f5beca
tcc6g7 <tcc6g7@mst.edu>
2013-12-02 18:18:37 -0600

    removed excess code

--- StayOpenCheckBoxMenuItem.java
+++ StayOpenCheckBoxMenuItem.java
@@ -19,7 +19,6 @@ import javax.swing.event.ChangeListener;
 public class StayOpenCheckBoxMenuItem extends JCheckBoxMenuItem {
 
   private static MenuElement[] path;
-
   {
     getModel().addChangeListener(new ChangeListener() {
 
@@ -33,52 +32,10 @@ public class StayOpenCheckBoxMenuItem extends JCheckBoxMenuItem {
   }
 
   /**
-   * @see JCheckBoxMenuItem#JCheckBoxMenuItem()
-   */
-  public StayOpenCheckBoxMenuItem() {
-    super();
-  }
-
-  /**
-   * @see JCheckBoxMenuItem#JCheckBoxMenuItem(Action)
-   */
-  public StayOpenCheckBoxMenuItem(Action a) {
-    super(a);
-  }
-
-  /**
-   * @see JCheckBoxMenuItem#JCheckBoxMenuItem(Icon)
-   */
-  public StayOpenCheckBoxMenuItem(Icon icon) {
-    super(icon);
-  }
-
-  /**
    * @see JCheckBoxMenuItem#JCheckBoxMenuItem(String)
    */
   public StayOpenCheckBoxMenuItem(String text) {
     super(text);
-  }
-
-  /**
-   * @see JCheckBoxMenuItem#JCheckBoxMenuItem(String, boolean)
-   */
-  public StayOpenCheckBoxMenuItem(String text, boolean selected) {
-    super(text, selected);
-  }
-
-  /**
-   * @see JCheckBoxMenuItem#JCheckBoxMenuItem(String, Icon)
-   */
-  public StayOpenCheckBoxMenuItem(String text, Icon icon) {
-    super(text, icon);
-  }
-
-  /**
-   * @see JCheckBoxMenuItem#JCheckBoxMenuItem(String, Icon, boolean)
-   */
-  public StayOpenCheckBoxMenuItem(String text, Icon icon, boolean selected) {
-    super(text, icon, selected);
   }
 
   /**
