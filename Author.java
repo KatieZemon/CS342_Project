229d2c102dba3571de39a01f994eae46efcb09fc
Katie Isbell <ktisbell@gmail.com>
2013-12-02 22:22:36 -0600

    Added comments

--- Author.java
+++ Author.java
@@ -55,13 +55,36 @@ public class Author extends KInternalFrame
 
             // TOM CLAY
             "TOM CLAY\n" +
+            "Contact:\n" +
+            "      Phone: 816-547-5127\n" +
+            "      Email:  thomas.clay@mst.edu\n" +
+            "Contributions:\n" +
+            "      -Created AlgorithmSelectionListener.java, FrameDisplayListener.java, and MenuBuilder.java  in order to improve the organization of our code for the greater goal of making the program more portable and easier to read.  \n" +
+            "      - StayOpenCheckBoxMenu so our check box menus could stay open while we are selecting algorithms.\n" +
+            "      -Implemented the following sorting algorithms:\n" +
+            "            -merge sort\n" +
+            "            -binary tree sort\n" +
+            "            -cycle sort\n" +
+            "            -stooge sort\n" +
+            "            -shell sort\n" +
+            "            -heap sort\n" +
+            "            -bucket sort\n" +
+            "            -trucket sort\n" +
+            "            -Tom sort\n" +
+            "            -bead sort\n" +
+            "            -counting sort\n" +
+            "Originality\n" +
+            "      -Reference number 4 under the list of references was used in order to create StayOpenCheckBoxMenu.\n" +
+            "      -TomSort and TrucketSort are originally created by me\n" +
+            "Learned:\n" +
+            "      -A lot of the different algorithms I implemented, excluding heap, binary tree, and merge.\n" +
             "\n\n\n" +
 
             // Katie Isbell
             "KATIE ISBELL\n" +
             "Contact:\n" +
             "      Phone: 251-709-0425\n" +
-            "      Email:  kjihdc@mst.edu\n\n" +
+            "      Email:  kjihdc@mst.edu\n" +
             "Contributions:\n" +
             "      -Set up the program with a menu bar and all needed menus and menu items.\n" +
             "      -Created Author.java, Problem.java, References.java, and Help.java.\n" +
@@ -76,20 +99,35 @@ public class Author extends KInternalFrame
             "            -cocktail sort\n" +
             "            -odd-even sort\n" +
             "            -gnome sort\n" +
-            "      -Wrote the User’s Manual except for information about the other authors\n\n" +
+            "      -Wrote the User’s Manual except for information about the other authors\n" +
             "Originality:\n" +
             "      Wikipedia was referenced in order to recall how each of these algorithms work\n" +
             "      Dr. Chaman Sabharwal’s notes about creating Rectangular bars and writing multithreaded programs" +
-            " were referenced\n\n" +
+            " were referenced\n" +
             "Learned:\n" +
-            "      Learned how to write multi-threaded code in Java\n" +
-            "      Refreshed my knowledge of all the sorting algorithms used in this program\n" +
-            "      Became more comfortable with using Github\n" +
-            "      Became familiar with the Trello application that was used for organizing the team’s checklist\n" +
+            "      -Learned how to write multi-threaded code in Java\n" +
+            "      -Refreshed my knowledge of all the sorting algorithms used in this program\n" +
+            "      -Became more comfortable with using Github\n" +
+            "      -Became familiar with the Trello application that was used for organizing the team’s checklist\n" +
             "\n\n\n" +
 
             // Westin Sykes
-            "WESTIN SYKES";
+            "WESTIN SYKES" +
+            "Contact:\n" +
+            "      Phone: 870-404-1413\n" +
+            "      Email: jws279@mst.edu\n" +
+            "Contributions:\n" +
+            "      -Shaped MainDisplay.java to work dynamically with sorts and made it to be easy to add new sorting algorithms.\n" +
+            "      -Wrote instructions to add new sorts to code.\n" +
+            "      -Implemented bubble sort and radix sort\n" +
+            "      -Implemented ability to vertically scale sort panels\n" +
+            "Originality:\n" +
+            "      -Used Wikipedia for remembering how each sorting algorithm worked.\n" +
+            "      -Referenced the following website to figure out how the Radix algorithm worked: " +
+            "http://gauss.ececs.uc.edu/Courses/C321/html/radix.java.html\n" +
+            "Learned:\n" +
+            "      How Radix sort works\n" +
+            "      Java threads experience\n";
 
     // Initialize our text area
     ta = new JTextArea(authorString, TA_WIDTH, TA_HEIGHT);
